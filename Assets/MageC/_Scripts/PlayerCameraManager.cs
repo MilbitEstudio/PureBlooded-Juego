b7d6847708f3b90325cb0a36c6fd3c64a106a6bc
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2025-05-05 06:10:18 +0200

    Suavizado_Camara

--- Assets/MageC/_Scripts/PlayerCameraManager.cs
+++ Assets/MageC/_Scripts/PlayerCameraManager.cs
@@ -3,74 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCameraManager : MonoBehaviour
-
 {
-
     public Transform PlayerBody;
-    public float MouseSensitivity;
+    public float MouseSensitivity = 100f;
+    public float Smooth = 10f;
 
-    public float Smooth;
+    float mouseX, mouseY;
+    float xClamp = 0f;
 
-    float XAxisClamp;
+    Quaternion camTargetRot;
+    Quaternion bodyTargetRot;
 
     void Start()
-
     {
-
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
+        camTargetRot = transform.localRotation;
+        bodyTargetRot = PlayerBody.localRotation;
     }
 
     void Update()
-
     {
+        mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
 
-        RotateCamera();
+        xClamp -= mouseY;
+        xClamp = Mathf.Clamp(xClamp, -90f, 90f);
 
+        camTargetRot = Quaternion.Euler(xClamp, 0f, 0f);
+
+        bodyTargetRot *= Quaternion.Euler(0f, mouseX, 0f);
+
+        // Aplicar suavizado
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, camTargetRot, Smooth * Time.deltaTime);
+        PlayerBody.localRotation = Quaternion.Slerp(PlayerBody.localRotation, bodyTargetRot, Smooth * Time.deltaTime);
     }
-
-    void RotateCamera()
-
-    {
-
-        float Mouse_X = Input.GetAxis("Mouse X");
-        float Mouse_Y = Input.GetAxis("Mouse Y");
-
-        float Rotation_Amount_X = Mouse_X * MouseSensitivity * Smooth;
-        float Rotation_Amount_Y = Mouse_Y * MouseSensitivity * Smooth;
-
-        XAxisClamp -= Rotation_Amount_Y;
-
-        Vector3 Target_Rotation_Cam = transform.rotation.eulerAngles;
-        Vector3 Target_Rotation_Body = PlayerBody.rotation.eulerAngles;
-
-        Target_Rotation_Cam.x -= Rotation_Amount_Y;
-        Target_Rotation_Cam.z = 0;
-        Target_Rotation_Body.y += Rotation_Amount_X;
-
-        if (XAxisClamp > 90)
-
-        {
-
-            XAxisClamp = 90;
-            Target_Rotation_Cam.x = 90;
-
-        }
-
-        else if (XAxisClamp < -90)
-
-        {
-
-            XAxisClamp = -90;
-            Target_Rotation_Cam.x = 270;
-
-        }
-
-
-        transform.rotation = Quaternion.Euler(Target_Rotation_Cam);
-        PlayerBody.rotation = Quaternion.Euler(Target_Rotation_Body);
-
-    }
-
 }
